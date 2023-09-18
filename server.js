27724dfa6ee3e06c67c3a14e31ef03e9cb7d94e9
Bibi <abdalla.corostl@gmailm>
2023-09-18 13:47:26 -0500

    my lil abdullahi

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware to serve static files
+// app.use('/js', express.static(path.join(__dirname, 'js')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
